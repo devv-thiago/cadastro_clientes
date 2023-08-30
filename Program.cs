5ef330b43004850bcec0607a4c9ef7e72950cd3b
devv-thiago <tfofano3@gmail.com>
2023-08-30 16:05:21 -0300

    Ajuste metodo de registro, listagem e remocao

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿internal class Program
+﻿using System.Reflection.Metadata.Ecma335;
+
+internal class Program
 {
 
     struct Cliente {
@@ -18,25 +20,31 @@
     enum Menu{Listagem = 1, Adicionar = 2, Remover = 3, Sair = 4 }
     private static void Main(string[] args)
     {
-        Console.WriteLine("Sistema de clientes - Bem vindo!");
-        Console.WriteLine("1-Listar clientes\n2-Adicionar\n3-Remover\n4-Sair");
-        int operacao = int.Parse(Console.ReadLine());
-        Menu opcao = (Menu)operacao;
         bool sairPrograma = false;
 
         while (!sairPrograma)
         {
+            Console.WriteLine("Sistema de clientes - Bem vindo!");
+            Console.WriteLine("1-Listar clientes\n2-Adicionar\n3-Remover\n4-Sair");
+            int operacao = int.Parse(Console.ReadLine());
+            Menu opcao = (Menu)operacao;
             switch (opcao)
             {
                 case Menu.Listagem:
                     listarRegistros();
+                    Console.WriteLine("\nPressione ENTER para voltar para o Menu principal.");
+                    Console.ReadLine();
+                    Console.Clear();
                     break;
                 case Menu.Adicionar:
                     adicionarRegistro();
+                    Thread.Sleep(1000);
+                    Console.Clear();
                     break;
                 case Menu.Remover:
-                    
-                    Console.WriteLine();
+                    removerRegistro();
+                    Thread.Sleep(1000);
+                    Console.Clear();
                     break;
                 case Menu.Sair:
                     Console.WriteLine();
@@ -48,9 +56,9 @@
     }
     static void listarRegistros()
     {
-        for (string cliente in clientes)
+        foreach(var cliente in clientes)
         {
-            Console.WriteLine(cliente);
+            Console.WriteLine($"Nome cliente: {cliente.nome} - E-mail cliente: {cliente.email} - Telefone cliente: {cliente.telefone}");
         }
     }
     static void adicionarRegistro()
@@ -64,21 +72,24 @@
         string telefoneCliente = Console.ReadLine();
         Cliente novoCliente = new Cliente(nomeCliente, emailCliente, telefoneCliente);
         clientes.Add(novoCliente);
+        Console.WriteLine("Novo cliente adicionado com sucesso!");
     }
     static void removerRegistro()
     {
         Console.WriteLine("--Exclusão de Clientes--");
         Console.Write("Digite o nome do cliente: ");
         string nomeCliente = Console.ReadLine();
-        string clienteRemocao = clientes.Find(cliente => cliente.nome == nomeCliente);
-        if (clienteRemocao == nomeCliente)
+
+        Cliente clienteRemocao = clientes.Find(cliente => cliente.nome == nomeCliente);
+
+        if (clienteRemocao.ToString != null)
         {
             clientes.Remove(clienteRemocao);
             Console.WriteLine("Registro removido com sucesso");
-            Console.Clear();
-        } else
+        }
+        else
         {
-            Console.WriteLine("Digite um nome válido!");
+            Console.WriteLine("Cliente não encontrado. Digite um nome válido!");
         }
     }
 }
